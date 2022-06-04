9392a37973622ad85bad749bca24ef6b58da0838
CaioProg <caioprog18@gmail.com>
2022-06-04 20:45:16 -0300

    Adicionando pesquisa a pagina

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,14 +20,25 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; } = default!;
+        public IList<Movie> Movie { get; set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
+            // using System.Linq;
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Movie = await _context.Movie.ToListAsync();
+                movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
